bda3335d1b43b89c0d8ca2c96f4d3172e9431809
unknown <irvaguileradev7@gmail.com>
2023-04-28 23:59:44 -0600

    CRUD Pisos Completo
    CRUD Para numeros de los pisos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FloorController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\TypeController;
@@ -21,3 +22,4 @@ Route::get('/', function () {
 
 Route::resource('rooms', RoomController::class);
 Route::resource('types', TypeController::class);
+Route::resource('floors', FloorController::class);
