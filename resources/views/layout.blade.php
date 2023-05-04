31768b4c80bd42e25ec64754ae0d0bef7d61c191
unknown <irvaguileradev7@gmail.com>
2023-05-03 23:36:09 -0600

    Layout modificado
    Modificaciones en el layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,78 +12,14 @@
 </head>
 
 <body>
-    {{--
-    <div class="container-fluid">
-        <div class="row flex-nowrap">
-            <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-                <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                    <a href="/"
-                        class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-5 d-none d-sm-inline">HotelGes</span>
-                    </a>
-                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-                        id="menu">
-                        <li>
-                            <a href="{{ route('rooms.index') }}" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-speedometer2"></i> <span
-                                    class="ms-1 d-none d-sm-inline">Habitaciones</span> </a>
 
-                        </li>
-                        <li>
-                            <a href="{{ route('types.index') }}" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Crear
-                                    tipo</span> </a>
-
-                        </li>
-                        <li>
-                            <a href="{{ route('floors.index') }}" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Crear
-                                     piso</span> </a>
-
-                        </li>
-
-                        <li>
-                            <a href="{{ route('guests.index') }}" class="nav-link px-0 align-middle">
-                                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Agregar huesped</span> </a>
-
-                        </li>
-
-                    </ul>
-                    <hr>
-
-                </div>
-            </div>
-            <!--Contenido de las vistas dentro del layout-->
-            <div class="col py-3">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <div class="container-fluid">
-                      <a class="navbar-brand" href="#">Navbar</a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                      </button>
-                      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                          <a class="nav-link active" aria-current="page" href="#">Home</a>
-                          <a class="nav-link" href="#">Features</a>
-                          <a class="nav-link" href="#">Pricing</a>
-                          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </div>
-                      </div>
-                    </div>
-                  </nav>
-                @yield('content')
-            </div>
-        </div>
-    </div>
---}}
     <div class="side-navbar active-nav d-flex justify-content-between flex-wrap flex-column" id="sidebar">
         <ul class="nav flex-column text-white w-100">
-          <a href="/"
-                        class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                        <span class="fs-5 d-none d-sm-inline">HotelGes</span>
-                    </a>
+            <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <span class="fs-5 d-none d-sm-inline">HotelGes</span>
+            </a>
             <li>
-              <hr>
+                <hr>
                 <a href="{{ route('rooms.index') }}" class="nav-link px-0 align-middle">
                     <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Habitaciones</span> </a>
 
@@ -108,18 +44,28 @@
 
             </li>
 
-            <span href="#" class="nav-link h4 w-100 mb-5">
-                <a href=""><i class="bx bxl-instagram-alt text-white"></i></a>
-                <a href=""><i class="bx bxl-twitter px-2 text-white"></i></a>
-                <a href=""><i class="bx bxl-facebook text-white"></i></a>
-            </span>
     </div>
 
     <!-- Main Wrapper -->
     <div class="p-1 my-container active-cont">
         <!-- Top Nav -->
         <nav class="navbar top-navbar navbar-light bg-light px-5">
-            <a class="btn border-0" id="menu-btn"><i class="bx bx-menu"></i></a>
+            <a class="btn border-0" id="menu-btn"><i class="bi bi-list"></i></a>
+            <div class="container">
+                <div class="navbar-nav">
+                    <span class="nav-item nav-link disabled">{{ Auth::user()->name }}</span>
+                </div>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-primary"
+                        onclick="event.preventDefault(); this.closest('form').submit();">
+                        Cerrar sesión
+                    </button>
+                </form>
+            </div>
+
+
+
         </nav>
         <!--End Top Nav -->
         @yield('content')
