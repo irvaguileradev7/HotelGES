b467e5ed68a25d6533f07271fa0d33cef75f0fcf
irvaguileradev7 <122749432+irvaguileradev7@users.noreply.github.com>
2023-05-26 10:35:32 -0600

    Comentario
    ya me acorde

--- resources/views/asignrooms/index.blade.php
+++ resources/views/asignrooms/index.blade.php
@@ -28,7 +28,7 @@
 
 
 
-
+				{{--Poner el contenido pero que este dentro del nuevo  estandar dinamico--}}
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12 margin-tb">
