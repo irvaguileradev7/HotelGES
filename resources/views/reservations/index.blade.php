5fc434d864b84947cd638a616392b046cccb200f
irvaguileradev7 <122749432+irvaguileradev7@users.noreply.github.com>
2023-06-04 14:46:20 -0600

    fechas mostrandose

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -26,44 +26,101 @@
         </form>
 
 
+
         @if ($reservations->count() > 0)
             <h2>Reservaciones existentes:</h2>
 
             @php
                 $currentMonth = null;
                 $currentYear = null;
+                $calendar = [];
             @endphp
 
-            <div class="calendar">
-                @foreach ($reservations as $reservation)
-                    @php
-                        $month = date('F', strtotime($reservation->time_from));
-                        $year = date('Y', strtotime($reservation->time_from));
-                    @endphp
+            @foreach ($reservations as $reservation)
+                @php
+                    $month = date('F', strtotime($reservation->time_from));
+                    $year = date('Y', strtotime($reservation->time_from));
+                    
+                    $calendar[$year][$month][] = $reservation;
+                @endphp
+            @endforeach
 
-                    @if ($currentMonth != $month || $currentYear != $year)
-                        @if (!is_null($currentMonth))
-                            </ul>
+            @foreach ($calendar as $year => $months)
+                <div class="year">
+                    <h3>{{ $year }}</h3>
+                    <div class="calendar">
+                        @foreach ($months as $month => $reservations)
+                            <div class="month">
+                                <h4>{{ $month }}</h4>
+                                <table>
+                                    <thead>
+                                        <tr>
+                                            <th>D</th>
+                                            <th>L</th>
+                                            <th>M</th>
+                                            <th>M</th>
+                                            <th>J</th>
+                                            <th>V</th>
+                                            <th>S</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @php
+                                            $firstDay = date('N', strtotime($year . '-' . $month . '-01'));
+                                            $totalDays = date('t', strtotime($year . '-' . $month . '-01'));
+                                            $currentDay = 1;
+                                            $weekDay = 1;
+                                        @endphp
+                                        <tr>
+                                            @while ($weekDay < $firstDay)
+                                                <td></td>
+                                                @php
+                                                    $weekDay++;
+                                                @endphp
+                                            @endwhile
+                                            @while ($currentDay <= $totalDays)
+                                                <td>
+                                                    <span>{{ $currentDay }}</span>
+                                                    @foreach ($reservations as $reservation)
+                                                        @php
+                                                            $reservationFrom = date('j', strtotime($reservation->time_from));
+                                                            $reservationTo = date('j', strtotime($reservation->time_to));
+                                                        @endphp
+                                                        @if ($currentDay >= $reservationFrom && $currentDay <= $reservationTo)
+                                                            <div class="reservation" style="background-color: red;">
+                                                                <span>{{ $reservation->time_from }}</span>
+                                                                <span>{{ $reservation->time_to }}</span>
+                                                            </div>
+                                                        @endif
+                                                    @endforeach
+                                                </td>
+                                                @php
+                                                    $currentDay++;
+                                                    $weekDay++;
+                                                @endphp
+                                                @if ($weekDay > 7)
+                                                    @php
+                                                        $weekDay = 1;
+                                                    @endphp
+                                        </tr>
+                                        <tr>
                         @endif
-
-                        <div class="month">
-                            <h3>{{ $month }} {{ $year }}</h3>
-                            <ul>
-                    @endif
-
-                    <li>Fecha de inicio: {{ $reservation->time_from }}</li>
-                    <li>Fecha de fin: {{ $reservation->time_to }}</li>
-                    <br>
-
-                    @php
-                        $currentMonth = $month;
-                        $currentYear = $year;
-                    @endphp
-                @endforeach
-
-                </ul>
-            </div>
-        @else
-            <p>No hay reservaciones existentes para este cuarto.</p>
-        @endif
-    @endsection
+            @endwhile
+            @while ($weekDay <= 7)
+                <td></td>
+                @php
+                    $weekDay++;
+                @endphp
+            @endwhile
+            </tr>
+            </tbody>
+            </table>
+    </div>
+    @endforeach
+    </div>
+    </div>
+    @endforeach
+@else
+    <p>No hay reservaciones existentes para este cuarto.</p>
+    @endif
+@endsection
