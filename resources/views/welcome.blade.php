48e86d77e8246364600e25f9b7d51b951caa2623
irvaguileradev7 <122749432+irvaguileradev7@users.noreply.github.com>
2023-12-10 16:01:39 -0600

    Cambios mayores

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <div class="container" style="margin-right: 200px, margin-left: 200px">
+    <div class="container" style= "margin-top:10%">
         <div class="row">
-            <div class="card" style="margin-right: 200px">
-
-                <div class="col">
-                    <div class="">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="space-padding">
                         <div class="">
                             <h1>Bienvenido, {{ Auth::user()->name }} </h1>
                         </div>
@@ -17,19 +16,24 @@
                             <img class="mod-size" src="{{ asset('/img/LogoHG.png') }}" width="400rem">
                             </a>
                         </div>
-
-
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="grid-container">
+                    <div class="container">
+                        <a href="{{ route('asignrooms.index') }}">
+                            <div class="btn btn-green">Habitaciones disponibles</div>
+                    </div>
+                </div>
+                <div class="grid-container">
+                    <div class="container">
+                        <a href="{{ route('guests.index') }}">
+                            <div class="btn btn-green">Lista de huespedes</div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="grid-container">
-                <div class="container">
-                    <a href="{{ route('asignrooms.index') }}">
-                        <div class="btn btn-green">Ver habitaciones disponibles</div>
-                </div>
-            </div>
-        </div>
+
     </div>
 @endsection
